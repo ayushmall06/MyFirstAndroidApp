f11c9b7c39a17540e8a8a5f684734eea80376408
Ayush Mall <ayushmall@Ayushs-MacBook-Pro.local>
2021-03-28 20:59:07 +0530

    App added

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView result;
+    EditText var1;
+    EditText var2;
+    Button calculate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        var1 = (EditText) findViewById(R.id.var1);
+        var2 = (EditText) findViewById(R.id.var2);
+        result = (TextView) findViewById(R.id.result);
+        calculate = (Button) findViewById(R.id.calculate);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(String.valueOf(var1.getText()));
+                int y = Integer.parseInt(String.valueOf(var2.getText()));
+                result.setText(String.valueOf(x+y));
+            }
+        });
+    }
+
+}
